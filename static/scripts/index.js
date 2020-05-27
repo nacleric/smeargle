06a9c2e6498f991562565114453b96b5fab326b4
Eric Ng <naclericny@gmail.com>
2020-05-26 20:56:47 -0400

    pictures are now ordered by date

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,27 +1,29 @@
 function loadPhotos() {
-  let gallery = document.getElementById("gallery");
+  let unsortedArray = [];
+  let gallery = document.querySelector("#gallery");
   for (let i = 0; i < localStorage.length; i++) {
     let key = localStorage.key(i);
     if (key !== "isDark") {
-      try {
-        let item = JSON.parse(localStorage.getItem(key));
-        let imgUrl = item.url;
-        console.log(imgUrl)
-        let imgTitle = item.title;
-        let newElement = `
-            <div class="gallery-photo-container">
-              <a href="${imgUrl}">
-                <img class="gallery-photo" src="${imgUrl}"></img>
-              </a>
-              <a class="delete-button"><i data-feather="x"></i></a>
-              <p class="gallery-photo-title">${imgTitle}</p>
-            </div>
-        `
-        gallery.innerHTML += newElement;
-      } catch (error) {
-        console.error(error);
-      }
+      unsortedArray[i] = JSON.parse(localStorage.getItem(key));
     }
+  }
+  
+  sortedArray = unsortedArray.sort((a, b) => (new Date(b.date) - new Date(a.date)));
+
+  for (let i = 0; i < sortedArray.length; i++) {
+    let item = sortedArray[i];
+    let imgUrl = item.url;
+    let imgTitle = item.title;
+    let newElement = `
+        <div class="gallery-photo-container">
+          <a href="${imgUrl}">
+            <img class="gallery-photo" src="${imgUrl}"></img>
+          </a>
+          <a class="delete-button"><i data-feather="x"></i></a>
+          <p class="gallery-photo-title">${imgTitle}</p>
+        </div>
+    `
+    gallery.innerHTML += newElement;
   }
 }
 
@@ -30,7 +32,7 @@ function bindDeleteToButtons() {
   for (let i = 0; i < deleteButtons.length; i++) {
     let button = deleteButtons[i]
     button.addEventListener("click", function() {
-      let gallery = document.getElementById("gallery");
+      let gallery = document.querySelector("#gallery");
       let galleryPhotoContainer = button.parentElement;
       gallery.removeChild(galleryPhotoContainer)
       let titleTag = galleryPhotoContainer.childNodes[5].innerHTML;
@@ -38,6 +40,15 @@ function bindDeleteToButtons() {
     })
   }
 }
+
+// function exportData() {
+//   let exportButton = document.getElementById("export")
+//   for (let i = 0; i < localStorage.length; i++) {
+//     console.log("pass");
+//     let key = localStorage.key(i);
+    
+//   }
+// }
 
 window.onload = function() {
   loadPhotos();
